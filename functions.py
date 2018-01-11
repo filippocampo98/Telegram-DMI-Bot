17b691408eaf5e02e266f8d3f9dbf5e66e30d904
Alessandro Maggio <alex.tkd.alex@gmail.com>
2018-01-11 23:06:22 +0100

    add try exception to prevent crash bot

--- functions.py
+++ functions.py
@@ -214,7 +214,15 @@ def callback(bot, update):
 
 			file1=drive2.CreateFile({'id':update.callback_query.data})
 			if file1['mimeType']=="application/vnd.google-apps.folder":
-				file_list2= drive2.ListFile({'q': "'"+file1['id']+"' in parents and trashed=false",'orderBy':'folder,title'}).GetList()
+				file_list2 = None
+
+				try:
+					file_list2 = drive2.ListFile({'q': "'"+file1['id']+"' in parents and trashed=false",'orderBy':'folder,title'}).GetList()
+				except Exception as e: 
+        			print("- Drive error: {}".format(e))
+					bot2.sendMessage(chat_id=update['callback_query']['from_user']['id'], text="Si è verificato un errore, ci scusiamo per il disagio. Contatta i devs. /help")
+					sys.exit(0)
+					
 				for file2 in file_list2:
 
 					fileN=""
