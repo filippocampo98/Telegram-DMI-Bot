340c9722fad4d01275aefebab0fd9fff1c47c665
Helias <stefanoborzi32@gmail.com>
2018-02-19 22:47:17 +0100

    Updated database structure

--- data/DMI_DB.sql
+++ data/DMI_DB.sql
@@ -6,20 +6,24 @@ CREATE TABLE IF NOT EXISTS `Chat_id_List` (
   `Nome` int(11) NOT NULL,
   `Cognome` int(11) NOT NULL,
   `E-mail` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 ALTER TABLE `Chat_id_List`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `Chat_id_List`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 -- stat_list
-CREATE TABLE stat_list (
+CREATE TABLE IF NOT EXISTS stat_list (
 	Type varchar(100),
 	chat_id int(100),
 	DateCommand DATE
+);
+
+-- subscriptions
+CREATE TABLE IF NOT EXISTS `subscriptions` (
+  `chatid` INT(11) NOT NULL,
+  `mensa` INT(11) NOT NULL,
+  `news` INT(11) NOT NULL
 );
