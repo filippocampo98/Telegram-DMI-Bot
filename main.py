e28bb1028464fefb3922dc2a231432621db941da
Helias <stefanoborzi32@gmail.com>
2019-01-28 01:29:55 +0100

    Removed the second CallbackQueryHandler association, close #50

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 from functions import *
 
-bot = telegram.Bot(TOKEN)
-
 with open('config/settings.yaml') as yaml_config:
 	config_map = yaml.load(yaml_config)
+
+bot = telegram.Bot(config_map["token"])
 
 def logging_message(bot, update):
 	try:
