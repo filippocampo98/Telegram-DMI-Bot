2167ff20d21bcfab2d6c34fa67524c59aa21fb31
Guberlo <salasero24@gmail.com>
2020-10-05 18:33:35 +0200

    added keyboard buttons

--- module/utils/keyboard_utils.py
+++ module/utils/keyboard_utils.py
@@ -4,7 +4,10 @@ from telegram import KeyboardButton, ReplyKeyboardMarkup
 def get_help_keyboard() -> ReplyKeyboardMarkup:
     kb = [
         [
-            KeyboardButton('❔ Help')
+            KeyboardButton('❔ Help'),
+            KeyboardButton('📫 Segnalazione Rappresentanti'),
+            KeyboardButton('📆 Aulario'),
+            KeyboardButton('☁️ Cloud')
         ],
     ]
     return ReplyKeyboardMarkup(kb, resize_keyboard=True)
