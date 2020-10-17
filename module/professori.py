0799f349fe699bc26d959133165259764c68be41
Guberlo <salasero24@gmail.com>
2020-10-17 18:33:45 +0200

    forgot to add this file in previous commit

--- module/professori.py
+++ module/professori.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from telegram import Update
+from telegram.ext import CallbackContext
+from module.shared import check_log
+
 import json
 import sqlite3
+
+def prof(update: Update, context: CallbackContext):
+    check_log(update, context, "prof")
+    message_text = prof_cmd(context.args)
+    if len(message_text) > 4096:
+        send_message(update, context, message_text)
+    else:
+        context.bot.sendMessage(chat_id=update.message.chat_id, text=message_text, parse_mode='Markdown')
 
 def prof_output(prof):
     output = "*Ruolo:* " + prof[1] + "\n"
