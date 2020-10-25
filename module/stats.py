3be6c3449ba19786dfcd0f914367e7d8bd6a55a9
TendTo <casablancaernesto@gmail.com>
2020-10-25 16:52:13 +0100

    add: /stats and /stats_top generate a graph

--- module/stats.py
+++ module/stats.py
@@ -1,10 +1,12 @@
 # Telegram
-from telegram import Update
+from telegram import Update, Bot
 from telegram.ext import CallbackContext
 
 # System libraries
 import sqlite3
+import os
 from datetime import date, timedelta
+import matplotlib.pyplot as plt
 
 def stats(update: Update, context: CallbackContext):
     if context.args:
@@ -27,9 +29,12 @@ def stats_gen(update: Update, context: CallbackContext, days):
         text += "Record di "+str(days)+" giorni:\n"
         query = "SELECT Type, n FROM (SELECT Type, COUNT(chat_id) as n FROM stat_list WHERE DateCommand > '"+ str(date.today()-timedelta(days=days)) + "' GROUP BY Type) ORDER BY n DESC;"
 
+    rows = conn.execute(query).fetchall()
+    conn.close()
+
     total = 0
     n_commands = 0
-    for row in conn.execute(query):
+    for row in rows:
         if str(row[0]) not in ("leiCheNePensaSignorina", "smonta_portoni", "santino", "bladrim", "prof_sticker"):
             text += f"{row[1]} : {row[0]}\n"
             total+= row[1]
@@ -37,7 +42,31 @@ def stats_gen(update: Update, context: CallbackContext, days):
     text += f"\nTotale: {total}\nMedia per comando: {round(total/n_commands, 2)}"
 
     context.bot.sendMessage(chat_id=chat_id, text=text)
-    conn.close()
+    send_graph(rows, context.bot, chat_id)
 
 def stats_tot(update: Update, context: CallbackContext):
     stats_gen(update, context, 0)
+
+# Create the graph and send it to the user
+def send_graph(rows: list, bot: Bot, chat_id: int):
+    # Prende solo i primi 10 valori
+    x = [v[0] for v in rows[:10]]
+    y = [v[1] for v in rows[:10]]
+
+    _, ax = plt.subplots()
+
+    # Imposta le labels per il grafico
+    ax.bar(x, y, align='center')
+    ax.set_title('Statistiche utilizzo comandi')
+    ax.set_ylabel('Utilizzi')
+    ax.set_xlabel('Comandi')
+
+    # Sistema la grafica e salva il file
+    plt.setp(ax.get_xticklabels(), rotation=30, horizontalalignment='center', fontsize='xx-small')
+    plt.tight_layout()
+    plt.savefig(str(chat_id))
+
+    # Invia l'immagine e successivamente la cancella
+    with open(str(chat_id) + ".png", "rb") as photo:
+        bot.send_photo(chat_id=chat_id, photo=photo)
+    os.unlink(str(chat_id) + ".png")
