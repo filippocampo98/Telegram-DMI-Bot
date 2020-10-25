ec1e4870c5dec4827fbdd8d82e39e4393ec54bb0
TendTo <casablancaernesto@gmail.com>
2020-10-25 14:44:50 +0100

    improve: /stat and /stat_tot commands

--- module/stats.py
+++ module/stats.py
@@ -7,9 +7,9 @@ import sqlite3
 from datetime import date, timedelta
 
 def stats(update: Update, context: CallbackContext):
-    if(len(update['message']['text'].split(' ')) == 2):
-        days = int(update['message']['text'].split(' ')[1])
-        if(days <= 0):
+    if context.args:
+        days = int(context.args[0])
+        if days <= 0:
             days = 30
     else:
         days = 30
@@ -18,19 +18,24 @@ def stats(update: Update, context: CallbackContext):
 def stats_gen(update: Update, context: CallbackContext, days):
     conn = sqlite3.connect('data/DMI_DB.db')
     chat_id = update.message.chat_id
-    query = ""
     text = ""
 
     if days == 0:
         text += "Record Globale:\n"
-        query = "SELECT Type, count(chat_id) FROM stat_list GROUP BY Type ORDER BY Type;"
+        query = "SELECT Type, n FROM (SELECT Type, COUNT(chat_id) as n FROM stat_list GROUP BY Type) ORDER BY n DESC;"
     else:
         text += "Record di "+str(days)+" giorni:\n"
-        query = "SELECT Type, count(chat_id) FROM stat_list WHERE DateCommand > '"+ str(date.today()-timedelta(days=days)) + "' GROUP BY Type ORDER BY Type;"
+        query = "SELECT Type, n FROM (SELECT Type, COUNT(chat_id) as n FROM stat_list WHERE DateCommand > '"+ str(date.today()-timedelta(days=days)) + "' GROUP BY Type) ORDER BY n DESC;"
 
+    total = 0
+    n_commands = 0
     for row in conn.execute(query):
-        if str(row[0]) != "leiCheNePensaSignorina" and str(row[0]) != "smonta_portoni" and str(row[0]) != "santino" and str(row[0]) != "bladrim" and str(row[0]) != "prof_sticker":
-            text += str(row[1]) + ": " + str(row[0]) + "\n"
+        if str(row[0]) not in ("leiCheNePensaSignorina", "smonta_portoni", "santino", "bladrim", "prof_sticker"):
+            text += f"{row[1]} : {row[0]}\n"
+            total+= row[1]
+            n_commands += 1
+    text += f"\nTotale: {total}\nMedia per comando: {round(total/n_commands, 2)}"
+
     context.bot.sendMessage(chat_id=chat_id, text=text)
     conn.close()
 
