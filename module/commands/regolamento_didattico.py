628df705334e361339a1a3f38ed83e5d84137aaa
Santo Cariotti <santo@dcariotti.me>
2021-04-28 21:13:40 +0200

    Fix "Regolamento didattico" for Maths classes (#167)
    * fix: "regolamento" callbacks for math degree
    
    The fact of this issue is the name of the dictionary keys. Python
    increasingly reads if-elif-else statements so, when it finds a good key
    to the "Regolamento didattico 2020/2021" it does not matter if that "regolamento"
    is for class L31 or L35. It returns `True` for the first good `if`.(Same
    for LM18 and LM40).
    
    So, it's better define different keys for a different class. Dictionary
    keys now end with `_L{class}` so Python can't be wrong!
    
    * fix(tests): press the 4 rules buttons
    
    After pressing the button to obtain a "Regolamento didattico" you get
    the "Regolamento didattico" of the current academic year.

--- module/commands/regolamento_didattico.py
+++ module/commands/regolamento_didattico.py
@@ -5,38 +5,38 @@ from telegram.ext import CallbackContext
 from module.shared import check_log
 
 reg_doc_triennale_L31 = {
-    'Regolamento Didattico 2020/2021': 'http://web.dmi.unict.it/sites/default/files/files/L%2031_Informatica%20AA%202020-21%20all\'albo.pdf',
-    'Regolamento Didattico 2019/2020': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%202019-20%20L%2031_Informatica.pdf',
-    'Regolamento Didattico 2018/2019': 'http://web.dmi.unict.it/sites/default/files/files/L%2031%20Informatica(1).pdf',
-    'Regolamento Didattico 2017/2018': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%20L%2031%20Informatica%202017-18.pdf',
-    'Regolamento Didattico 2016/2017': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%20L%2031%20Informatica%202016-17.pdf',
-    'Regolamento Didattico 2015/2016': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%20L%2031%20Informatica%202015-16.pdf',
-    'Regolamento Didattico 2014/2015': 'http://web.dmi.unict.it/sites/default/files/files/Didattica%20Programmata%20e%20elenco%20propedeuticit%C3%A0%202014-2015.pdf',
-    'Regolamento Didattico 2013/2014': 'http://web.dmi.unict.it/sites/default/files/files/regolamentoDidattico_L31_Informatica_1314.pdf',
-    'Regolamento Didattico 2012/2013': 'http://web.dmi.unict.it/sites/default/files/files/regolamentoDidattico_L31_Informatica_1213.pdf',
+    'Regolamento Didattico 2020/2021_L31': 'http://web.dmi.unict.it/sites/default/files/files/L%2031_Informatica%20AA%202020-21%20all\'albo.pdf',
+    'Regolamento Didattico 2019/2020_L31': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%202019-20%20L%2031_Informatica.pdf',
+    'Regolamento Didattico 2018/2019_L31': 'http://web.dmi.unict.it/sites/default/files/files/L%2031%20Informatica(1).pdf',
+    'Regolamento Didattico 2017/2018_L31': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%20L%2031%20Informatica%202017-18.pdf',
+    'Regolamento Didattico 2016/2017_L31': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%20L%2031%20Informatica%202016-17.pdf',
+    'Regolamento Didattico 2015/2016_L31': 'http://web.dmi.unict.it/sites/default/files/files/Regolamento%20L%2031%20Informatica%202015-16.pdf',
+    'Regolamento Didattico 2014/2015_L31': 'http://web.dmi.unict.it/sites/default/files/files/Didattica%20Programmata%20e%20elenco%20propedeuticit%C3%A0%202014-2015.pdf',
+    'Regolamento Didattico 2013/2014_L31': 'http://web.dmi.unict.it/sites/default/files/files/regolamentoDidattico_L31_Informatica_1314.pdf',
+    'Regolamento Didattico 2012/2013_L31': 'http://web.dmi.unict.it/sites/default/files/files/regolamentoDidattico_L31_Informatica_1213.pdf',
 }
 
 reg_doc_magistrale_LM18 = {
-    'Regolamento Didattico 2020/2021_m': 'http://web.dmi.unict.it/sites/default/files/Regolamento%20Didattico%20LM%2018%202021.pdf',
-    'Regolamento Didattico 2019/2020_m': 'http://web.dmi.unict.it/sites/default/files/Regolamento%20Didattico%20LM18%201920_0.pdf',
-    'Regolamento Didattico 2018/2019_m': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/Regolamento%20Didattico%20LM18%201819.pdf',
-    'Regolamento Didattico 2017/2018_m': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/Regolamento%20Didattico%20LM18%201718.pdf',
-    'Regolamento Didattico 2016/2017_m': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2018%20Informatica_1617.pdf',
-    'Regolamento Didattico 2015/2016_m': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/Regolamento%20Didattico%20LM18%201516.pdf'
+    'Regolamento Didattico 2020/2021_LM18': 'http://web.dmi.unict.it/sites/default/files/Regolamento%20Didattico%20LM%2018%202021.pdf',
+    'Regolamento Didattico 2019/2020_LM18': 'http://web.dmi.unict.it/sites/default/files/Regolamento%20Didattico%20LM18%201920_0.pdf',
+    'Regolamento Didattico 2018/2019_LM18': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/Regolamento%20Didattico%20LM18%201819.pdf',
+    'Regolamento Didattico 2017/2018_LM18': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/Regolamento%20Didattico%20LM18%201718.pdf',
+    'Regolamento Didattico 2016/2017_LM18': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2018%20Informatica_1617.pdf',
+    'Regolamento Didattico 2015/2016_LM18': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/Regolamento%20Didattico%20LM18%201516.pdf'
 }
 
 reg_doc_triennale_L35 = {
-    'Regolamento Didattico 2020/2021': 'http://web.dmi.unict.it/sites/default/files/files/L%2035_Matematica_2020_21approvato.pdf',
-    'Regolamento Didattico 2019/2020': 'http://web.dmi.unict.it/sites/default/files/files/L%2035_Matematica.pdf',
-    'Regolamento Didattico 2018/2019': 'http://web.dmi.unict.it/sites/default/files/files/Regol%20L35_1819%20approvato%20Senato.pdf',
-    'Regolamento Didattico 2017/2018': 'http://web.dmi.unict.it/sites/default/files/files/L%2035%20ufficiale1718.pdf',
-    'Regolamento Didattico 2016/2017': 'http://web.dmi.unict.it/sites/default/files/files/regolamento1617pubbldallAteneo.pdf',
+    'Regolamento Didattico 2020/2021_L35': 'http://web.dmi.unict.it/sites/default/files/files/L%2035_Matematica_2020_21approvato.pdf',
+    'Regolamento Didattico 2019/2020_L35': 'http://web.dmi.unict.it/sites/default/files/files/L%2035_Matematica.pdf',
+    'Regolamento Didattico 2018/2019_L35': 'http://web.dmi.unict.it/sites/default/files/files/Regol%20L35_1819%20approvato%20Senato.pdf',
+    'Regolamento Didattico 2017/2018_L35': 'http://web.dmi.unict.it/sites/default/files/files/L%2035%20ufficiale1718.pdf',
+    'Regolamento Didattico 2016/2017_L35': 'http://web.dmi.unict.it/sites/default/files/files/regolamento1617pubbldallAteneo.pdf',
 }
 
 reg_doc_magistrale_LM40 = {
-    'Regolamento Didattico 2020/2021_m': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2040_Matematica%202020-21%20approvato%20Senato.pdf',
-    'Regolamento Didattico 2019/2020_m': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2040_Matematica%2019-20%20approvato%20senato.pdf',
-    'Regolamento Didattico 2018/2019_m': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2040%20Matematica%202018-19%20approvato%20senato.pdf',
+    'Regolamento Didattico 2020/2021_LM40': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2040_Matematica%202020-21%20approvato%20Senato.pdf',
+    'Regolamento Didattico 2019/2020_LM40': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2040_Matematica%2019-20%20approvato%20senato.pdf',
+    'Regolamento Didattico 2018/2019_LM40': 'http://web.dmi.unict.it/sites/default/files/documenti_sito/LM%2040%20Matematica%202018-19%20approvato%20senato.pdf',
 }
 
 REGOLAMENTI = {
@@ -123,6 +123,6 @@ def get_reg_keyboard(reg_doc: dict = None) -> InlineKeyboardMarkup:
             InlineKeyboardButton('L-35', callback_data='reg_button_triennale_L35'),
             InlineKeyboardButton('LM-40', callback_data='reg_button_magistrale_LM40'),
         ]])
-    keyboard = [[InlineKeyboardButton(r.replace('_m', ''), callback_data=r)] for r in reg_doc]
+    keyboard = [[InlineKeyboardButton(r.split('_')[0], callback_data=r)] for r in reg_doc]
     keyboard.append([InlineKeyboardButton('Indietro', callback_data='reg_button_home')])  # back button
     return InlineKeyboardMarkup(keyboard)
--- tests/test_bot.py
+++ tests/test_bot.py
@@ -302,32 +302,33 @@ async def test_regolamentodidattico_cmd(client: TelegramClient):
     """
     conv: Conversation
     async with client.conversation(bot_tag, timeout=TIMEOUT) as conv:
-
-        await conv.send_message("/regolamentodidattico")  # send a command
-        resp: Message = await conv.get_response()
-
-        assert resp.text
-
         buttons = (
             "reg_button_triennale_L31",
             "reg_button_triennale_L35",
-            "reg_button_home",
             "reg_button_magistrale_LM18",
             "reg_button_magistrale_LM40",
         )
 
         for button in buttons:
-            await resp.click(data=button)  # click the button
-            resp: Message = await conv.get_edit()
+            await conv.send_message("/regolamentodidattico")  # send a command
+            resp: Message = await conv.get_response()
 
             assert resp.text
 
-        await resp.click(data="Regolamento Didattico 2020/2021_m")  # click "Regolamento" button
+            await resp.click(data=button)  # click the button
+            resp = await conv.get_edit()
 
-        resp: Message = await conv.get_edit()
+            assert resp.text
 
-        assert resp.text
+            class_num = button.split('_')[-1]
+            rule_button_text = "Regolamento Didattico 2020/2021_{}".format(class_num)
 
-        resp: Message = await conv.get_response()
+            await resp.click(data=rule_button_text)  # click "Regolamento" button
 
-        assert resp.document
+            resp = await conv.get_edit()
+
+            assert resp.text
+
+            resp = await conv.get_response()
+
+            assert resp.document
